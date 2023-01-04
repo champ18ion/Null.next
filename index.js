b0e2d36eedd43b85bd21426e63e6154e05fc54b3
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2023-01-04 10:19:31 +0530

    added form

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
 
