08b0936632c1481f801fa5bf158cef63e8806761
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2023-01-11 11:38:55 +0530

    added basic flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -72,12 +72,14 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success','logged in successfully')
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout(function(err){
         if(err){console.log("error");}
+        req.flash('success','logged out')
         return res.redirect('/');
 
     });
--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const flash = require('connect-flash')
+const flashWare = require('./config/middleware')
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+
 
 app.use(express.urlencoded({extended:true}));
 
@@ -54,6 +57,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash())
+app.use(flashWare.setFlash)
 
 // use express router
 app.use('/', require('./routes'));
